670150fbe859b9b44629513cf715ff72a9376d39
srijanpusp <puspsrijan@gmail.com>
2024-11-08 20:55:02 +0530

    added live score board and some comments

--- game.cpp
+++ game.cpp
@@ -30,37 +30,37 @@ void screen_init()
   top_limit = csbi.srWindow.Top;
   bottom_limit = csbi.srWindow.Bottom;
 }
-
-
+//a data type to define position of stuff
 struct point
 {
   int xCoordinate, yCoordinate;
-  point(){}
-  point(int x, int y)
+  point(){} //default constructor
+  point(int x, int y) //parameterised constructor
   {
     xCoordinate = x;
     yCoordinate = y;
   }
 };
 
+// a class to define the properties of snake such as length, position, direction etc
 class Snake
 {
   private:
     int length;
     char direction;
   public:
-    point body[Max_Length]; 
-    Snake(int x, int y)
+    point body[Max_Length];  //array of type points (coordinates)
+    Snake(int x, int y)   //constructor that creates snake of length 1 and sets it in motion 
     {
-      length = 1;
+      length = 1;  
       body[0] = point(x, y);
       direction = RIGHT;
     }
     int getLength()
     {
-      return length;
+      return length;  //this one will be used later
     }
-    void changeDirection(char newDirection)
+    void changeDirection(char newDirection)  //this one changes the direction of snake as per input
     {
       if(newDirection == UP && direction != DOWN)
       {
@@ -79,13 +79,13 @@ class Snake
         direction = newDirection;
       }
     }
-    bool move(point food)
+    bool move(point food)  
     {
-      for(int i = length-1; i>0; i--)
+      for(int i = length-1; i>0; i--) //this one makes the snake move by constantly changing the position of every 'o' in snake
       {
         body[i] = body[i-1];
       }
-      switch (direction)
+      switch (direction)    // switch case, to figure out movement by taking input from keyboard 
       {
         int val;
         case UP:
@@ -106,20 +106,20 @@ class Snake
           break;
       }
       //game over
-      for(int i=1; i<length; i++)
+      for(int i=1; i<length; i++)  //if snake bites itself
       {
         if(body[0].xCoordinate == body[i].xCoordinate && body[0].yCoordinate == body[i].yCoordinate)
         {
           return false;
         }
-        if (body[0].xCoordinate < left_limit || body[0].xCoordinate >= right_limit ||
+        if (body[0].xCoordinate < left_limit || body[0].xCoordinate >= right_limit ||       //if goes through wall
             body[0].yCoordinate < top_limit || body[0].yCoordinate >= bottom_limit) 
         {
               return false;
         }
       }
       //eat and grow
-      if(food.xCoordinate == body[0].xCoordinate && food.yCoordinate == body[0].yCoordinate)      
+      if(food.xCoordinate == body[0].xCoordinate && food.yCoordinate == body[0].yCoordinate)   //if eats food, it grows   
       {
         body[length] = point(body[length-1].xCoordinate, body[length-1].yCoordinate);
         length++;
@@ -128,56 +128,62 @@ class Snake
     }
 };
 
-class Board
+class Board  //contains, controles and displays all elements such as snake, food, etc
 {
   private:
-    Snake *snake;
-    const char snakeBody = 'o';
-    point food;
-    int score;
-    const char FOOD = 'o';
+    Snake *snake;  //dynamically creates snake
+    const char snakeBody = 'o';  //so the snake goes ooooooooo
+    point food;  //creating food of point type so it has position on board
+    int score;  //hmmmm
+    const char FOOD = '+';   //so food is visible
   public:
-    Board()
+    Board()   //constructor
     {
-      spawnFood();
-      snake = new Snake(2,2);
-      score = 0;
+      spawnFood();  //random food spawn goooooo
+      snake = new Snake(2,2);    //as the board gets created, we need a new snake at 2,2 coordinates and 
+      score = 0; //new score 00
     }
     ~Board()
     {
-      delete snake;
+      delete snake;  //since snake was created in heap memory
     }
     int getScore()
     {
-      return score;
+      return score;   //will cone to use later
     }
-    void spawnFood()
+    void printScore()
+    {
+      gotoXY(consoleWidth/2, 0);
+      cout<<"score: "<< getScore();
+
+    }
+    void spawnFood()  // this makes sure food spawns randomly
     {
       int x = rand() % consoleWidth;
       int y = rand() % consoleHeight;
       food = point(x, y);
     }
-    void gotoXY(int x, int y)
+    void gotoXY(int x, int y)  //sets the cursor
     {
       COORD coord;
       coord.X = x;
       coord.Y = y;
       SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
     }
-    void draw()
+    void draw()  //whatever you see on screen, is being done by this huehue
     {
-      system("cls");
-      for(int i=0; i<snake->getLength(); i++)
+      system("cls");  //clers the screen everytime
+      for(int i=0; i<snake->getLength(); i++)   //prints the snake 
       {
-        gotoXY(snake->body[i].xCoordinate, snake->body[i].yCoordinate);
+        gotoXY(snake->body[i].xCoordinate, snake->body[i].yCoordinate);  //so basically, this moves the cursor, which prints snake at every x,y
         cout<<snakeBody;
       }
-      gotoXY(food.xCoordinate, food.yCoordinate);
+      gotoXY(food.xCoordinate, food.yCoordinate);    //sends cursor where food is
       cout<<FOOD;
     }
-    bool update()
+    bool update()    //keeps checking if you alive and maintains score and stuff
     {
-      bool valid = snake->move(food);
+      bool valid = snake->move(food);  //passes on the move function with food, which checks if we need to increase score or die
         if(valid == false)
         {
           return false;
@@ -191,7 +197,7 @@ class Board
         return true;
     }
   
-      void getInput()
+      void getInput()  //sets up for wasd key work
       {
         if(kbhit())
         {
@@ -225,8 +231,10 @@ int main()
   {
     board->getInput();
     board->draw();
+    board->printScore();
     Sleep(100);
   }
-  cout<<"YOU DIE!!!!!!";
+  cout<<"YOU DIE!!!!!!"<<endl;
+  cout<<"score: "<<board->getScore();
   return 0;
 }
